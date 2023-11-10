e3fee6cfffddfd820cf6d83b0eda0ee5aa0a74ce
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-10 15:41:47 +0100

    prova funzionalità variabile prezzoViaggio: funzionante ma assenza di decimali. Difficoltà nella lettura certificazione ufficiale su mdn e W3S

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@
 // calcolare prezzo totale del viaggio in base ai km (0.21 euro a km)
 // sconto del 20% sui minorenni e 40% sui over 65
 
+console.log('ciaooo')
+
+const kmDaPercorrere = parseInt(prompt('Quanti km vuoi percorrere?'));
+const etàPasseggero = parseInt(prompt('Quanti anni ha il passeggero?'));
+
+let prezzoViaggio = parseInt (kmDaPercorrere * 0.21);
+
+console.log(prezzoViaggio)
+
+if (etàPasseggero < 18) {
+    prezzoViaggio = parseInt()
+}

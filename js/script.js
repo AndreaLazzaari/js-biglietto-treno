76f705dbed7206bdfb5193063c06d56278e91e13
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-10 16:24:42 +0100

    inserita struttura variabile if in script.js

--- js/script.js
+++ js/script.js
@@ -5,12 +5,22 @@
 console.log('ciaooo')
 
 const kmDaPercorrere = parseInt(prompt('Quanti km vuoi percorrere?'));
-const etàPasseggero = parseInt(prompt('Quanti anni ha il passeggero?'));
+const etaPasseggero = parseInt(prompt('Quanti anni ha il passeggero?'));
 
-let prezzoViaggio = parseInt (kmDaPercorrere * 0.21);
+let prezzoViaggio = kmDaPercorrere * 0.21;
 
 console.log(prezzoViaggio)
 
-if (etàPasseggero < 18) {
-    prezzoViaggio = parseInt()
+if (etaPasseggero < 18) {
+    const sconto = prezzoViaggio * 20 / 100;
+    // prezzoViaggio = prezzoViaggio - sconto;
+    prezzoViaggio -= sconto;
+    console.log(sconto,prezzoViaggio);
+} else if (etaPasseggero > 65) {
+    const sconto = prezzoViaggio * 40 / 100;
+    prezzoViaggio -= sconto;
+    console.log(sconto,prezzoViaggio)
+} else {
+    console.log(prezzoViaggio)
 }
+
